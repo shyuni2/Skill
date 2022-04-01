88011a517e8a01b67d322c9c0538890cce2a94ef
이지구 <rose97_@naver.com>
2022-04-01 19:01:35 +0900

    1

--- lsh/Sample3D-FBX Loader/FbxLoader.cpp
+++ lsh/Sample3D-FBX Loader/FbxLoader.cpp
@@ -1,1 +1,79 @@
 #include "FbxLoader.h"
+
+bool FbxLoader::Load(std::string filename)
+{
+	bool bRet = m_pFbxImporter->Initialize(filename.c_str());
+	bRet = m_pFbxImporter->Import(m_pFbxScene);
+	m_pRootNode = m_pFbxScene->GetRootNode();
+	PreProcess(m_pRootNode, nullptr);
+	for (int iObj = 0; iObj < m_ObjList.size(); iObj++)
+	{
+		ParseMesh(m_ObjList[iObj]);
+	}
+	return true;
+}
+void FbxLoader::PreProcess(FbxNode* node, FbxNode* parent)
+{
+	FbxMesh* pMesh = node->GetMesh();
+	if (pMesh)
+	{
+		FbxObj* fbx = new FbxObj;
+		fbx->m_pFbxParent = parent;
+		fbx->m_pFbxNode = node;
+		m_ObjList.push_back(fbx);
+	}
+	int iNumChild = node->GetChildCount();
+	for (int iNode = 0; iNode < iNumChild; iNode++)
+	{
+		FbxNode* child = node->GetChild(iNode);
+		PreProcess(child, node);
+	}
+}
+void FbxLoader::ParseMesh(FbxObj* pObject)
+{
+
+}
+std::string FbxLoader::ParseMaterial(FbxSurfaceMaterial* pMtrl)
+{
+
+}
+
+void FbxLoader::ReadTextureCoord(FbxMesh* pFbxMesh, FbxLayerElementUV* pUVSet, int vertexIndex, int uvIndex, FbxVector2& uv)
+{
+
+}
+FbxColor FbxLoader::ReadColor(const FbxMesh* mesh, DWORD dwVertexColorCount, FbxLayerElementVertexColor* pVertexColorSet, DWORD dwDCCIndex, DWORD dwVertexIndex)
+{
+
+}
+FbxVector4 FbxLoader::ReadNormal(const FbxMesh* mesh, int controlPointIndex, int vertexCounter)
+{
+
+}
+
+bool FbxLoader::Init()
+{
+	m_pFbxManager = FbxManager::Create();
+	m_pFbxImporter = FbxImporter::Create(m_pFbxManager, "");
+	m_pFbxScene = FbxScene::Create(m_pFbxScene, "");
+	return true;
+}
+bool FbxLoader::Frame()
+{
+	return true;
+}
+bool FbxLoader::Render()
+{
+	return true;
+}
+bool FbxLoader::Release()
+{
+	for (int iObj = 0; iObj < m_ObjList.size(); iObj++)
+	{
+		m_ObjList[iObj]->Release();
+	}
+	m_pFbxScene->Destroy();
+	m_pFbxImporter->Destroy();
+	m_pFbxManager->Destroy();
+	return true;
+}
--- lsh/Sample3D-FBX Loader/FbxLoader.h
+++ lsh/Sample3D-FBX Loader/FbxLoader.h
@@ -1,5 +1,39 @@
 #pragma once
+#include "Obj3D.h"
+#include <fbxsdk.h>
+
+class FbxObj : public Obj3D
+{
+public:
+	FbxNode* m_pFbxParent = nullptr;
+	FbxNode* m_pFbxNode = nullptr;
+	FbxObj* m_pParentObj = nullptr;
+	std::wstring m_szTexFileName;
+public:
+	bool SetIndexData() { return true; }
+};
+
 class FbxLoader
 {
+public:
+	FbxManager* m_pFbxManager; // FBX SDK 관리자 객체 생성
+	FbxImporter* m_pFbxImporter; // 씬을 가져오는기능
+	FbxScene* m_pFbxScene; // 씬 가져오기
+	FbxNode* m_pRootNode; // 루트노드
+	std::vector<FbxObj*>  m_ObjList;
+public:
+	virtual bool Load(std::string filename);
+	virtual void PreProcess(FbxNode* node, FbxNode* parent); // 전처리
+	virtual void ParseMesh(FbxObj* pObject); 
+	std::string ParseMaterial(FbxSurfaceMaterial* pMtrl); 
+public:
+	void ReadTextureCoord(FbxMesh* pFbxMesh, FbxLayerElementUV* pUVSet, int vertexIndex, int uvIndex, FbxVector2& uv);
+	FbxColor ReadColor(const FbxMesh* mesh, DWORD dwVertexColorCount, FbxLayerElementVertexColor* pVertexColorSet, DWORD dwDCCIndex, DWORD dwVertexIndex);
+	FbxVector4 ReadNormal(const FbxMesh* mesh, int controlPointIndex, int vertexCounter);
+public:
+	virtual bool Init();
+	virtual bool Frame();
+	virtual bool Render();
+	virtual bool Release();
 };
 
